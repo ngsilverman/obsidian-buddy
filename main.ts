3726bd01450c16ed0847ca39d83d3b2e07f8d18f
Nathanael Silverman <nathanael@hey.com>
2024-02-09 09:56:54 -0500

    Add file selection modal

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, MarkdownRenderer, MarkdownView, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, MarkdownRenderer, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile, ToggleComponent } from 'obsidian';
 import OpenAI from "openai";
 
 // Remember to rename these classes and interfaces!
@@ -77,9 +77,7 @@ const messageToMd = (m: Message): string => {
 
 function removeLines(s: string, start: number, end: number) {
 	const lines = s.split('\n');
-	console.log(lines)
 	lines.splice(start, end - start + 1);
-	console.log(lines)
 	return lines.join('\n');
 }
 
@@ -115,38 +113,7 @@ export default class BuddyPlugin extends Plugin {
 			checkCallback: (checking: boolean) => {
 				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
 				if (markdownView) {
-					if (!checking) {
-						const md = markdownViewToMd(markdownView, true);
-						const messages = gptMessages(md);
-
-						const fileName = markdownView.file?.name;
-						const folder = markdownView.file?.parent?.path;
-						console.log(markdownView.file);
-						const systemPrompt =
-							`You are a friendly AI assistant integrated inside of an Obsidian Markdown note named "${fileName}" in folder "${folder}".` +
-							" User messages constitute the entire document, interspersed with your own responses." +
-							" Feel free to use Markdown in your responses or anything that is compatible with Obsidian or Obsidian plugins.";
-						messages.unshift({ role: "system", content: systemPrompt });
-
-						console.log(messages)
-						openai.chat.completions.create({
-							messages: messages,
-							model: 'gpt-3.5-turbo',
-						}).then((completion) => {
-							const message = completion.choices[0].message;
-							const messageMd = messageToMd(message);
-							console.log(messageMd);
-
-							const editor = markdownView.editor;
-							const lastLine = editor.lineCount() - 1;
-							const lastCh = editor.getLine(lastLine).length;
-							const endPos = { line: lastLine, ch: lastCh };
-							console.log(endPos);
-
-							editor.replaceRange("\n" + messageMd, endPos);
-						});
-					}
-
+					if (!checking) this.chat(markdownView);
 					return true;
 				}
 			}
@@ -175,6 +142,73 @@ export default class BuddyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	private getLinks(file: TFile): { frontmatterLinks: string[], links: string[], backlinks: string[] } {
+		const cache = this.app.metadataCache.getFileCache(file);
+		const links = cache?.links?.map(l => l.link) || [];
+		const frontmatterLinks = cache?.frontmatterLinks?.map(l => l.link) || [];
+		// @ts-ignore
+		const backlinks = Object.keys(this.app.metadataCache.getBacklinksForFile(file)?.data) || [];
+		return { frontmatterLinks, links, backlinks };
+	}
+
+	private getLinkedFiles(file: TFile, depth: number = 1): TFile[] {
+		console.log('getLinkedFiles', file, depth);
+		if (depth < 1) return [];
+
+		const cache = this.app.metadataCache;
+		const { frontmatterLinks, links, backlinks } = this.getLinks(file);
+		let files = frontmatterLinks.concat(links).concat(backlinks)
+			.map(link => cache.getFirstLinkpathDest(link, file.path))
+			.filter(f => f != null) as TFile[];
+		// Remove duplicates
+		files = [...new Set(files)];
+
+		if (depth > 1) {
+			files = files.flatMap(f => [f, ...this.getLinkedFiles(f, depth - 1)])
+		}
+
+		// Remove duplicates, again
+		return [...new Set(files)];
+	}
+
+	private chat(markdownView: MarkdownView) {
+		const file = markdownView.file!;
+		const md = markdownViewToMd(markdownView, true);
+
+		const linkedFiles = this.getLinkedFiles(file, 2);
+		new FileSelectionModal(this.app, linkedFiles, selectedFiles => {
+			console.log('selectedFiles', selectedFiles);
+		}).open();
+
+		const messages = gptMessages(md);
+
+		const fileName = markdownView.file?.name;
+		const folder = markdownView.file?.parent?.path;
+		const systemPrompt =
+			`You are a friendly AI assistant integrated inside of an Obsidian Markdown note named "${fileName}" in folder "${folder}".` +
+			" User messages constitute the entire document, interspersed with your own responses." +
+			" Feel free to use Markdown in your responses or anything that is compatible with Obsidian or Obsidian plugins.";
+		messages.unshift({ role: "system", content: systemPrompt });
+
+		console.log(messages)
+		// openai.chat.completions.create({
+		// 	messages: messages,
+		// 	model: 'gpt-3.5-turbo',
+		// }).then((completion) => {
+		// 	const message = completion.choices[0].message;
+		// 	const messageMd = messageToMd(message);
+		// 	console.log(messageMd);
+
+		// 	const editor = markdownView.editor;
+		// 	const lastLine = editor.lineCount() - 1;
+		// 	const lastCh = editor.getLine(lastLine).length;
+		// 	const endPos = { line: lastLine, ch: lastCh };
+		// 	console.log(endPos);
+
+		// 	editor.replaceRange("\n" + messageMd, endPos);
+		// });
+	}
 }
 
 class BuddySettingTab extends PluginSettingTab {
@@ -198,5 +232,75 @@ class BuddySettingTab extends PluginSettingTab {
 					this.plugin.settings.openAIApiKey = value;
 					await this.plugin.saveSettings();
 				}));
+	}
+}
+
+class FileSelectionModal extends Modal {
+
+	private files: TFile[];
+	private onSubmit: (selectedFiles: TFile[]) => void;
+
+	private fileToggles: Map<TFile, ToggleComponent>;
+
+	constructor(app: App, files: TFile[], onSubmit: (selectedFiles: TFile[]) => void) {
+		super(app);
+		this.files = files;
+		this.onSubmit = onSubmit;
+	}
+
+	onOpen() {
+		this.fileToggles = new Map();
+
+		const { contentEl } = this;
+		contentEl.createEl("h2", { text: "Related files to include" });
+
+		this.addActions(contentEl);
+
+		for (const file of this.files) {
+			new Setting(contentEl)
+				.setName(file.basename)
+				.setTooltip(file.path)
+				.addToggle(toggle => {
+					this.fileToggles.set(file, toggle);
+				});
+		}
+
+		this.addActions(contentEl);
+	}
+
+	onClose() {
+		let { contentEl } = this;
+		contentEl.empty();
+	}
+
+	private addActions(contentEl: HTMLElement) {
+		new Setting(contentEl)
+			.addButton(button => button
+				.setButtonText("Select All")
+				.onClick(_e => {
+					for (const [_file, toggle] of this.fileToggles) {
+						toggle.setValue(true);
+					}
+				})
+			)
+			.addButton(button => button
+				.setButtonText("Select None")
+				.onClick(_e => {
+					for (const [_file, toggle] of this.fileToggles) {
+						toggle.setValue(false);
+					}
+				})
+			)
+			.addButton(button => button
+				.setCta()
+				.setButtonText("Submit")
+				.onClick(_e => {
+					this.close();
+					const selectedFiles = [...this.fileToggles]
+						.filter(([_file, toggle]) => toggle.getValue())
+						.map(([file, _toggle]) => file);
+					this.onSubmit(selectedFiles);
+				})
+			);
 	}
 }
